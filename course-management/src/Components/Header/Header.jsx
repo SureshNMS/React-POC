c24354c4421474fa83b330c14c28bfb25b9c9970
SureshNMS <sureshbabu.pammi@neudesic.com>
2025-07-05 20:56:53 +0530

    added mobile view navbar

--- course-management/src/Components/Header/Header.jsx
+++ course-management/src/Components/Header/Header.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link, NavLink } from "react-router-dom";
 import Logo from '../../Assets/course-management-logo-dark.png';
 import LoginButton from "../AuthButtons/LoginButton";
 import NavItem from "./NavList"; 
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (  
     <div className="m-0 p-2">
       {/* Banner */}
@@ -20,15 +21,40 @@ function Header() {
               className="h-auto w-[100px] mr-3"
             />
           </div>
-          <nav className="flex space-x-4 items-center text-sm">
+          <nav className="hidden md:flex space-x-4 items-center text-sm">
             <NavItem to="/" end>Home</NavItem>
             <NavItem to="/course">Course</NavItem>
-            <NavItem to="aboutus">About us</NavItem>
-            <NavItem to="contact">Contact us</NavItem>
+            <NavItem to="aboutus">About Us</NavItem>
+            <NavItem to="contact">Contact Us</NavItem>
           </nav>
 
           {/* Login and Signup Buttons */}
           <LoginButton />
+
+          {/* Hamburger */}
+          <div className="md:hidden ml-1 mt-1">
+            <button onClick={() => setMenuOpen(!menuOpen)} className="text-gray-700 focus:outline-none">
+              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                {menuOpen ? (
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                ) : (
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                )}
+              </svg>
+            </button>
+          </div>
+
+          {/* Mobile Menu */}
+        {menuOpen && (
+          <div className="md:hidden flex px-5 py-2.5 absolute right-0 top-[70px] bg-[#f2f2f2] text-right gap-5 w-full z-40 justify-end items-center flex-wrap text-xs">
+            <Link to="/" className="block text-black font-medium bg-gray-100 rounded">
+              Home
+            </Link>
+            <Link to="/courses" className="block hover:text-orange-500">Course</Link>
+            <Link to="/about" className="block hover:text-orange-500">About Us</Link>
+            <Link to="/contact" className="block hover:text-orange-500">Contact Us</Link>
+          </div>
+        )}
 
         </header>
       </div>
